7817505bddb339c8aa95818c98203890b93891bc
Oege Hiddema <oegehiddema@gmail.com>
2020-11-25 11:54:40 +0100

    workouts show page

--- resources/views/workouts/index.blade.php
+++ resources/views/workouts/index.blade.php
@@ -3,22 +3,6 @@
 @section('content')
 
 <div class="container">
-   <table class="table table-sm">
-      <thead class="thead-light">
-         <tr>
-            <th>Id</th>
-            <th>Date</th>
-         </tr>
-      </thead>
-      <tbody>
-         @foreach ($workouts as $workout)
-            <tr>
-               <td>{{$workout->id}}</td>
-               <td><a href="/workouts/{{$workout->id}}">{{$workout->date}}</a></td>
-            </tr>
-         @endforeach
-      </tbody>
-   </table>
 </div>
 
 @endsection
