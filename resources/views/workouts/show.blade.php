238ee51c7ef0472d0e973393c38d789b9030144a
Oege Hiddema <oegehiddema@gmail.com>
2020-12-02 08:47:39 +0100

    confirm on delete

--- resources/views/workouts/show.blade.php
+++ resources/views/workouts/show.blade.php
@@ -2,11 +2,27 @@
 
 @section('content')
 
+<script>
+
+$(function(){
+
+   $(".delWorkoutlog").on("submit", function(){
+      return confirm("Are you sure you want to delete this exercise?");
+   });
+
+   $(".delWorkout").on("submit", function(){
+      return confirm("Are you sure you want to delete this workout?");
+   });
+
+}); 
+
+</script>
+
 <div class="container">
    <p>workout-id: {{$workout->id}}</p>
    <p>workout-date: {{$workout->date}}</p>
 
-   <form action="/workouts/{{$workout->id}}" method="POST">
+   <form class="delWorkout" action="/workouts/{{$workout->id}}" method="POST">
       @csrf
       @method('DELETE')
       <button class="btn btn-danger mb-2" type="submit" title="delete">Delete this workout</button>
@@ -26,7 +42,7 @@
          <tr>
             <td><a class="btn btn-primary" href="/workoutlogs/{{$workoutlog->id}}/edit">Edit</a></td>
             <td>
-               <form action="/workoutlogs/{{$workoutlog->id}}" method="POST">
+               <form class="delWorkoutlog" action="/workoutlogs/{{$workoutlog->id}}" method="POST">
                   @csrf
                   @method('DELETE')
                   <button class="btn btn-danger mb-2" type="submit" title="delete">Delete</button>
