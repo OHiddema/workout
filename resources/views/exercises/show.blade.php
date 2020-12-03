866a61b41e6ddaa0f3b9c5294f89b74d5e34aafc
Oege Hiddema <oegehiddema@gmail.com>
2020-12-03 19:51:54 +0100

    improve layout

--- resources/views/exercises/show.blade.php
+++ resources/views/exercises/show.blade.php
@@ -1,26 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<a class="btn btn-primary" href="/exercises">All exercises</a><br>
 
-<div class="container rounded border border-dark pb-2 mt-2" style="background-color: rgb(180, 234, 255)">
-   <h1>{{$exercise->name}}</h1>
-   <p>Equipment: {{$exercise->equipment->name}}</p> 
-   <p>
+<div class="container">
+   <a class="btn btn-primary" href="/exercises">All exercises</a><br>
+   <div class="container rounded border border-dark pb-2 mt-2" style="background-color: rgb(180, 234, 255)">
+      <h1>{{$exercise->name}}</h1>
+      Equipment:
+      <ul>
+         <li>{{$exercise->equipment->name}}</li>
+      </ul>
       Bodyparts:
-      @foreach ($exercise->bodyparts as $bodypart)
-         {{$bodypart->name}}
+      <ul>
+         @foreach ($exercise->bodyparts as $bodypart)
+         <li>{{$bodypart->name}}</li>
       @endforeach
-   </p>
+      </ul>
 
-   <a class="btn btn-primary btn-sm" href="/exercises/{{$exercise->id}}/edit">Edit</a>
-   <div class="d-inline-block">
-      <form action="/exercises/{{$exercise->id}}" method="POST">
-      @csrf
-      @method('DELETE')
-      <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
-      </form>
-   </div>
+      <a class="btn btn-primary btn-sm" href="/exercises/{{$exercise->id}}/edit">Edit</a>
+      <div class="d-inline-block">
+         <form action="/exercises/{{$exercise->id}}" method="POST">
+         @csrf
+         @method('DELETE')
+         <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
+         </form>
+      </div>
+   </div>   
 </div>
 
 @endsection
