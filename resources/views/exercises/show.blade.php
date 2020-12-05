6528ecc2e17d74977d2a724cb95cb135de4284e3
Oege Hiddema <oegehiddema@gmail.com>
2020-12-05 14:21:48 +0100

    add confirmation alert box before deleting exercise

--- resources/views/exercises/show.blade.php
+++ resources/views/exercises/show.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
+<script>
+
+   $(function(){
+   
+      $(".delExercise").on("submit", function(){
+         return confirm("Are you sure you want to delete this exercise?");
+      });
+   
+   }); 
+   
+   </script>
+   
 <div class="container">
    <a class="btn btn-primary" href="/exercises">All exercises</a><br>
    <div class="container rounded border border-dark pb-2 mt-2" style="background-color: rgb(180, 234, 255)">
@@ -19,7 +31,7 @@
 
       <a class="btn btn-primary btn-sm" href="/exercises/{{$exercise->id}}/edit">Edit</a>
       <div class="d-inline-block">
-         <form action="/exercises/{{$exercise->id}}" method="POST">
+         <form class="delExercise" action="/exercises/{{$exercise->id}}" method="POST">
          @csrf
          @method('DELETE')
          <button class="btn btn-danger btn-sm" type="submit" title="delete">Delete</button>
