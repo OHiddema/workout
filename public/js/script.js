d368ed9e8d71a5bb8d0b139a8657b9c13bca4069
Oege Hiddema <oegehiddema@gmail.com>
2020-12-18 10:12:19 +0100

    replace depricated jQuery function

--- public/js/script.js
+++ public/js/script.js
@@ -5,27 +5,27 @@ function handleRating() {
    if ($('#rating').val() < 4) {$('#star4').addClass('checked');}
    if ($('#rating').val() < 5) {$('#star5').addClass('checked');}
 
-   $('#star1').click(function() {
+   $('#star1').on('click',function() {
       $('#star1').removeClass('checked');
       $('#star2,#star3,#star4,#star5').addClass('checked');
       $('#rating').val(1);
    })
-   $('#star2').click(function() {
+   $('#star2').on('click',function() {
       $('#star1,#star2').removeClass('checked');
       $('#star3,#star4,#star5').addClass('checked');
       $('#rating').val(2);
    })
-   $('#star3').click(function() {
+   $('#star3').on('click',function() {
       $('#star1,#star2,#star3').removeClass('checked');
       $('#star4,#star5').addClass('checked');
       $('#rating').val(3);
    })
-   $('#star4').click(function() {
+   $('#star4').on('click',function() {
       $('#star1,#star2,#star3,#star4').removeClass('checked');
       $('#star5').addClass('checked');
       $('#rating').val(4);
    })
-   $('#star5').click(function() {
+   $('#star5').on('click',function() {
       $('#star1,#star2,#star3,#star4,#star5').removeClass('checked');
       $('#rating').val(5);
    })
