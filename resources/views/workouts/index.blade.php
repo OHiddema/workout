b8ccf2a4881ebb21a39efffab151f80e924f8bb3
Oege Hiddema <oegehiddema@gmail.com>
2020-12-19 13:51:25 +0100

    beautify code

--- resources/views/workouts/index.blade.php
+++ resources/views/workouts/index.blade.php
@@ -103,191 +103,160 @@ Date.prototype.alt_getDay = function () {
 
 var Cal = function(divId) {
 
-//Store div id
-this.divId = divId;
+  //Store div id
+  this.divId = divId;
 
-// Days of week, starting on Sunday
-this.DaysOfWeek = [
-  'Mon',
-  'Tue',
-  'Wed',
-  'Thu',
-  'Fri',
-  'Sat',
-  'Sun',
-];
+  // Days of week, starting on Sunday
+  this.DaysOfWeek = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
 
-// Months, stating on January
-this.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+  // Months, stating on January
+  this.Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
 
-// Set the current month, year
-var d = new Date();
-// ############# ADDED ############################
-d.setMonth( d.getMonth() + <?php echo $months ?> );
-// ############# ADDED ############################
-
-this.currMonth = d.getMonth();
-this.currYear = d.getFullYear();
-this.currDay = d.getDate();
+  // Set the current month, year
+  var d = new Date();
+  d.setMonth( d.getMonth() + <?php echo $months ?> );
+  this.currMonth = d.getMonth();
+  this.currYear = d.getFullYear();
+  this.currDay = d.getDate();
 
 };
 
 // Goes to next month
 Cal.prototype.nextMonth = function() {
-// if ( this.currMonth == 11 ) {
-//   this.currMonth = 0;
-//   this.currYear = this.currYear + 1;
-// }
-// else {
-//   this.currMonth = this.currMonth + 1;
-// }
-// this.showcurr();
-// ############# ADDED ############################
-$('#months').val(parseInt($('#months').val())+1);
-$('#monthswitch').submit();
-// ############# ADDED ############################
+  $('#months').val(parseInt($('#months').val())+1);
+  $('#monthswitch').submit();
 };
 
 // Goes to previous month
 Cal.prototype.previousMonth = function() {
-// if ( this.currMonth == 0 ) {
-//   this.currMonth = 11;
-//   this.currYear = this.currYear - 1;
-// }
-// else {
-//   this.currMonth = this.currMonth - 1;
-// }
-// this.showcurr();
-// ############# ADDED ############################
-$('#months').val(parseInt($('#months').val())-1);
-$('#monthswitch').submit();
-// ############# ADDED ############################
+  $('#months').val(parseInt($('#months').val())-1);
+  $('#monthswitch').submit();
 };
 
 // Show current month
 Cal.prototype.showcurr = function() {
-this.showMonth(this.currYear, this.currMonth);
+  this.showMonth(this.currYear, this.currMonth);
 };
 
 // Show month (year, month)
 Cal.prototype.showMonth = function(y, m) {
 
-var d = new Date()
-// First day of the week in the selected month
-, firstDayOfMonth = new Date(y, m, 1).alt_getDay()
-// Last day of the selected month
-, lastDateOfMonth =  new Date(y, m+1, 0).getDate()
-// Last day of the previous month
-, lastDayOfLastMonth = m == 0 ? new Date(y-1, 12, 0).getDate() : new Date(y, m, 0).getDate();
+  var d = new Date()
+  // First day of the week in the selected month
+  , firstDayOfMonth = new Date(y, m, 1).alt_getDay()
+  // Last day of the selected month
+  , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
+  // Last day of the previous month
+  , lastDayOfLastMonth = m == 0 ? new Date(y-1, 12, 0).getDate() : new Date(y, m, 0).getDate();
 
 
-var html = '<table>';
+  var html = '<table>';
 
-// Write selected month and year
-html += '<thead><tr>';
-html += '<td colspan="7"><button id="btnPrev" type="button">Prev</button>' + this.Months[m] + ' ' + y + '<button id="btnNext" type="button">Next</button></td>';
-html += '</tr></thead>';
+  // Write selected month and year
+  html += '<thead><tr>';
+  html += '<td colspan="7"><button id="btnPrev" type="button">Prev</button>' + this.Months[m] + ' ' + y + '<button id="btnNext" type="button">Next</button></td>';
+  html += '</tr></thead>';
 
 
-// Write the header of the days of the week
-html += '<tr class="days">';
-for(var i=0; i < this.DaysOfWeek.length;i++) {
-  html += '<td>' + this.DaysOfWeek[i] + '</td>';
-}
-html += '</tr>';
-
-// Write the days
-var i=1;
-do {
-
-  var dow = new Date(y, m, i).alt_getDay();
-
-  // If Sunday, start new row
-  if ( dow == 0 ) {
-    html += '<tr>';
+  // Write the header of the days of the week
+  html += '<tr class="days">';
+  for(var i=0; i < this.DaysOfWeek.length;i++) {
+    html += '<td>' + this.DaysOfWeek[i] + '</td>';
   }
-  // If not Sunday but first day of the month
-  // it will write the last days from the previous month
-  else if ( i == 1 ) {
-    html += '<tr>';
-    var k = lastDayOfLastMonth - firstDayOfMonth+1;
-    for(var j=0; j < firstDayOfMonth; j++) {
-      html += '<td class="not-current">' + k + '</td>';
-      k++;
+  html += '</tr>';
+
+  // Write the days
+  var i=1;
+  do {
+
+    var dow = new Date(y, m, i).alt_getDay();
+
+    // If Sunday, start new row
+    if ( dow == 0 ) {
+      html += '<tr>';
     }
-  }
-
-  // Write the current day in the loop
-  var chk = new Date();
-  var chkY = chk.getFullYear();
-  var chkM = chk.getMonth();
-  if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
-    // ##### added id ##################
-    html += '<td class="today" id="day' + i + '">' + i + '</td>';
-  } else {
-    html += '<td class="normal" id="day' + i + '">' + i + '</td>';
-    // ##### added id ##################
-  }
-  // If Saturday, closes the row
-  if ( dow == 6 ) {
-    html += '</tr>';
-  }
-  // If not Saturday, but last day of the selected month
-  // it will write the next few days from the next month
-  else if ( i == lastDateOfMonth ) {
-    var k=1;
-    for(dow; dow < 6; dow++) {
-      html += '<td class="not-current">' + k + '</td>';
-      k++;
+    // If not Sunday but first day of the month
+    // it will write the last days from the previous month
+    else if ( i == 1 ) {
+      html += '<tr>';
+      var k = lastDayOfLastMonth - firstDayOfMonth+1;
+      for(var j=0; j < firstDayOfMonth; j++) {
+        html += '<td class="not-current">' + k + '</td>';
+        k++;
+      }
     }
-  }
 
-  i++;
-}while(i <= lastDateOfMonth);
+    // Write the current day in the loop
+    var chk = new Date();
+    var chkY = chk.getFullYear();
+    var chkM = chk.getMonth();
+    if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
+      // ##### added id ##################
+      html += '<td class="today" id="day' + i + '">' + i + '</td>';
+    } else {
+      html += '<td class="normal" id="day' + i + '">' + i + '</td>';
+      // ##### added id ##################
+    }
+    // If Saturday, closes the row
+    if ( dow == 6 ) {
+      html += '</tr>';
+    }
+    // If not Saturday, but last day of the selected month
+    // it will write the next few days from the next month
+    else if ( i == lastDateOfMonth ) {
+      var k=1;
+      for(dow; dow < 6; dow++) {
+        html += '<td class="not-current">' + k + '</td>';
+        k++;
+      }
+    }
 
-// Closes table
-html += '</table>';
+    i++;
+  }while(i <= lastDateOfMonth);
 
-// Write HTML to the div
-document.getElementById(this.divId).innerHTML = html;
+  // Closes table
+  html += '</table>';
+
+  // Write HTML to the div
+  document.getElementById(this.divId).innerHTML = html;
 };
 
 // On Load of the window
 window.onload = function() {
 
-// Start calendar
-var c = new Cal("divCal");			
-c.showcurr();
+  // Start calendar
+  var c = new Cal("divCal");			
+  c.showcurr();
 
-// Bind next and previous button clicks
-getId('btnNext').onclick = function() {
-  c.nextMonth();
-};
-getId('btnPrev').onclick = function() {
-  c.previousMonth();
-};
+  // Bind next and previous button clicks
+  getId('btnNext').onclick = function() {
+    c.nextMonth();
+  };
+  getId('btnPrev').onclick = function() {
+    c.previousMonth();
+  };
 }
 
 // Get element by id
 function getId(id) {
-return document.getElementById(id);
+  return document.getElementById(id);
 }
 </script>
 
 <!--################## ADDED #########################-->
 @foreach ($workouts as $workout)
-<script>
-  $(function(){
-    d = new Date(<?php echo 1000*strtotime($workout->date) ?>);
-    day = d.getDate();
-    if ($('#day' + day + ' a').length) {
-      $('#day' + day).html($('#day' + day).html() + ', <a href="/workouts/{{$workout->id}}">' + day + '</a>');
-    } else {
-      $('#day' + day).html('<a href="/workouts/{{$workout->id}}">' + day + '</a>');
-    }
-    $('#day' + day).addClass('trainingDay');
-  }); 
-</script>
+  <script>
+    $(function(){
+      d = new Date(<?php echo 1000*strtotime($workout->date) ?>);
+      day = d.getDate();
+      if ($('#day' + day + ' a').length) {
+        $('#day' + day).html($('#day' + day).html() + ', <a href="/workouts/{{$workout->id}}">' + day + '</a>');
+      } else {
+        $('#day' + day).html('<a href="/workouts/{{$workout->id}}">' + day + '</a>');
+      }
+      $('#day' + day).addClass('trainingDay');
+    }); 
+  </script>
 @endforeach
 <!--################## ADDED #########################-->
 
