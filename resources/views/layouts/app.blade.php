67bc4d516bb10e1d46adfa5b2b05fe59969f1622
Oege Hiddema <oegehiddema@gmail.com>
2020-12-21 14:27:57 +0100

    make navbar fixed-top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,10 +28,10 @@
     <link href="{{ asset('css/template.css') }}" rel="stylesheet">
 
 </head>
-<body>
+<body style="padding-top: 65px">
     
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar fixed-top navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
