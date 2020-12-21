6f832ebf05732a0dcc5da32b42264a8d1b22d014
Oege Hiddema <oegehiddema@gmail.com>
2020-12-21 14:18:23 +0100

    discard Bootstrap sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
-{{-- <div class="container">
-    <a class="btn btn-primary" href="/workouts">Workouts</a>
-    <a class="btn btn-primary" href="/exercises">Exercises</a>
-    <a class="btn btn-primary" href="/analysis/exercise">Analysis</a>
-</div> --}}
-
 <div class="cards-1">
     <div class="container">
         <div class="row">
